d6825f84c6ad83c006acabeb971019c424f9cefa
k-metra <metrakurt@gmail.com>
2025-09-22 18:29:40 +0800

    feat: add login API and update dashboard view with authentication

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns =[
-    path("", dashboard_view, name="dashboard")
+    path("", dashboard_view, name="dashboard"),
+    path("login-api/", dashboard_login_api, name="dashboard-login-api"),
+    path("login/", dashboard_login_view, name="dashboard-login"),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -2,9 +2,28 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from attendance.models import attendance
 
+from django.contrib.auth import authenticate, login
+from django.http import JsonResponse
+
 # temporarily comment as we are in testing
 # TODO: DO NOT FORGET TO UN-COMMENT THIS DECORATOR KURT !!!!!
-# @login_required
+@login_required
 def dashboard_view(request):
     data = attendance.objects.all()
     return render(request, "dashboard/dashboard.html", {"data_list": data})
+
+def dashboard_login_api(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return JsonResponse({"status": True, "message": "Login successful."})
+        else:
+            return JsonResponse({"status": False, "message": "Invalid username or password."})
+    return JsonResponse({"status": False, "message": "Invalid request method."})
+
+def dashboard_login_view(request):
+    return render(request, "dashboard/login.html")
