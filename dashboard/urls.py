654000815199d6fac325bce6bd2053c8bed14ea5
k-metra <metrakurt@gmail.com>
2025-09-23 10:56:26 +0800

    feat: add clear path to dashboard URL routing

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -6,4 +6,5 @@ urlpatterns =[
     path("login-api/", dashboard_login_api, name="dashboard-login-api"),
     path("login/", dashboard_login_view, name="dashboard-login"),
     path("logout/", dashboard_logout, name="dashboard-logout"),
+    path("clear/", dashboard_clear, name="dashboard-clear"),
 ]
