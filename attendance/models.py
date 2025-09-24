945d37918d2e4399e6146b9dcb0c27d65b06d78a
k-metra <metrakurt@gmail.com>
2025-09-24 16:37:20 +0800

    feat: implement log_id to ip_log attendance model

--- attendance/models.py
+++ attendance/models.py
@@ -10,3 +10,4 @@ class attendance(models.Model):
 class ip_log(models.Model):
     ip_address = models.GenericIPAddressField()
     timestamp = models.DateTimeField(auto_now_add=True)
+    log_id = models.IntegerField()
